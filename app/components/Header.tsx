e7b8828a4dcfa24fa46ec2203371f100c76f8d42
NiravRathwa <niravrathwa10@gmail.com>
2024-09-04 16:45:57 +0530

    Add scroll-triggered animations to home page sections

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -51,7 +51,6 @@ const Header = (props: Props) => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
-  console.log(isMobile);
   const toggle = () => {
     setOpen(!open);
   };
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Image from "next/image";
 import Header from "./components/Header";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
 const containerVariants = {
   visible: {
     transition: {
@@ -9,85 +11,136 @@ const containerVariants = {
     },
   },
 };
+
 export default function Home() {
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const isInView1 = useInView(ref1, { amount: 0.5 });
+  const isInView2 = useInView(ref2, { amount: 0.5 });
+  const isInView3 = useInView(ref3, { amount: 0.5 });
+
   return (
     <>
-      <section>
-        <div className="relative w-full h-screen overflow-hidden  ">
-          <Header />
-          <div className="absolute inset-0 bg-custom-gradient z-0"></div>
-          {/* <video
-          autoPlay
-          muted
-          loop
-          className="absolute top-1/2 left-1/2 w-auto h-auto min-w-full min-h-full object-cover transform -translate-x-1/2 -translate-y-1/2 opacity-50"
+      <section className="relative w-full h-screen overflow-hidden" ref={ref1}>
+        <Header />
+        <div className="absolute inset-0 bg-custom-gradient z-0"></div>
+        <motion.div
+          className="relative z-10 flex flex-col items-start justify-normal gap-5 w-full h-full text-white md:mt-8 p-8"
+          variants={containerVariants}
+          initial="hidden"
+          animate={isInView2 ? "visible" : "hidden"}
+        >
+          <motion.div
+            className="md:w-2/4"
+            initial={{ opacity: 0, y: 20 }}
+            animate={isInView1 ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
           >
-          <source src="/videos/video.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video> */}
-          <motion.div className="relative z-10 flex flex-col items-start justify-normal  gap-5 w-full h-full text-white md:mt-8 p-8">
-            <motion.div
-              className="md:w-2/4"
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 0.2 }}
-            >
-              <p className="mt-4 text-lg md:text-2xl">
-              tenetur molestias, facilis cum eius quod placeat officiis
-                perferendis! Veniam qui cumque facere.
-              </p>
-            </motion.div>
-            <motion.div
-              className="md:w-2/4"
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 0.7 }}
-            >
-              <h1 className="text-2xl md:text-4xl font-bold">Welcome </h1>
-              <p className="mt-4 text-lg md:text-2xl">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
-                numquam, veniam laboriosam nulla reiciendis, a similique officia
-                
-              </p>
-            </motion.div>
+            <p className="mt-4 text-lg md:text-2xl">
+              Hello molestias, facilis cum eius quod placeat officiis
+              perferendis! Veniam qui cumque facere.
+            </p>
           </motion.div>
-        </div>
+          <motion.div
+            className="md:w-2/4"
+            initial={{ opacity: 0, y: 20 }}
+            animate={isInView1 ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+            transition={{ duration: 0.5, delay: 0.7 }}
+          >
+            <h1 className="text-2xl md:text-4xl font-bold">Welcome </h1>
+            <p className="mt-4 text-lg md:text-2xl">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
+              numquam, veniam laboriosam nulla reiciendis, a similique officia
+            </p>
+          </motion.div>
+        </motion.div>
       </section>
-
-      <section className="w-full bg-[url('/example.png')] bg-cover bg-center h-screen ">
+      <section
+        ref={ref2}
+        className="w-full bg-[url('/example.png')] bg-cover bg-center h-screen"
+      >
         <motion.div
           variants={containerVariants}
+          initial="hidden"
+          animate={isInView2 ? "visible" : "hidden"}
           className="relative z-10 flex flex-col items-start justify-center gap-5 w-full h-full text-white p-8"
         >
           <motion.div
-              className="md:w-2/4"
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 0.2 }}
-            >
-              <p className="mt-4 text-lg md:text-2xl">
-                Hello molestias, facilis cum eius quod placeat officiis
-                perferendis! Veniam qui cumque facere.
-              </p>
-            </motion.div>
-            <motion.div
-              className="md:w-2/4"
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 0.7 }}
-            >
-              <h1 className="text-2xl md:text-4xl font-bold">Welcome </h1>
-              <p className="mt-4 text-lg md:text-2xl">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
-                numquam, veniam laboriosam nulla reiciendis, a similique officia
-                
-              </p>
-            </motion.div>
+            className="md:w-2/4"
+            initial={{ opacity: 0, y: 20 }}
+            animate={isInView2 ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+          >
+            <p className="mt-4 text-lg md:text-2xl">
+              Hello molestias, facilis cum eius quod placeat officiis
+              perferendis! Veniam qui cumque facere.
+            </p>
+          </motion.div>
+          <motion.div
+            className="md:w-2/4"
+            initial={{ opacity: 0, y: 20 }}
+            animate={isInView2 ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+            transition={{ duration: 0.5, delay: 0.7 }}
+          >
+            <h1 className="text-2xl md:text-4xl font-bold">Welcome </h1>
+            <p className="mt-4 text-lg md:text-2xl">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
+              numquam, veniam laboriosam nulla reiciendis, a similique officia
+            </p>
+          </motion.div>
         </motion.div>
       </section>
-      <section className="h-screen grid grid-cols-2 flex-col md:flex-row ">
-        <div className="bg-[url('/example.png')] "></div>
-        <div className="bg-[url('/example.png')] "></div>
+
+      <section
+        ref={ref3}
+        className="h-screen grid md:grid-cols-2 grid-cols-1 "
+      >
+       <motion.div
+    className="bg-[url('/example.png')] bg-cover bg-center rounded-md"
+    initial={{ opacity: 0, y: 20 }}
+    animate={isInView3 ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+    transition={{ duration: 0.5, delay: 0.2 }}
+  >
+    <h3 className="text-lg md:text-2xl font-semibold text-white p-4">
+      Services
+    </h3>
+    <motion.div className="flex justify-start items-center flex-col relative top-1/4 md:top-1/2 transform -translate-y-1/4 md:-translate-y-1/2 w-3/4 md:w-1/2 p-2">
+      <p className="text-white text-sm md:text-base mb-4">
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+      </p>
+      <motion.button
+        className="!place-self-start bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+      >
+        View Our Services
+      </motion.button>
+    </motion.div>
+  </motion.div>
+
+  <motion.div
+    className="bg-[url('/example.png')] bg-cover bg-center rounded-md"
+    initial={{ opacity: 0, y: 20 }}
+    animate={isInView3 ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+    transition={{ duration: 0.5, delay: 0.2 }}
+  >
+    <h3 className="text-lg md:text-2xl font-semibold text-white p-4">
+      Services
+    </h3>
+    <motion.div className="flex justify-start items-center flex-col relative top-1/4 md:top-1/2 transform -translate-y-1/4 md:-translate-y-1/2 w-3/4 md:w-1/2 p-2">
+      <p className="text-white text-sm md:text-base mb-4">
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+      </p>
+      <motion.button
+        className="!place-self-start bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+      >
+        View Our Services
+      </motion.button>
+    </motion.div>
+  </motion.div>
       </section>
     </>
   );
